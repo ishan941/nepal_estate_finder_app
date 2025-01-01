dda917d22c3da8bca63d52368e2d95a9ed80673c
Ishan Shrestha <iahan.esan18@gmail.com>
2025-01-01 10:56:40 +0545

    fix: fixed signup funcanility

--- lib/core/api_const.dart
+++ lib/core/api_const.dart
@@ -1,7 +1,7 @@
 class Api {
   static const String baseUrl = "https://1a04-27-34-73-167.ngrok-free.app/";
 
-  static const String loginApi = 'api/signin';
+  static const String loginApi = 'api/auth/signin';
   static const String signUpUserApi = 'api/auth/signup';
   static const String addResturentApi = "resturent/createResturent";
   static const String getResturentApi = "table/getTable";
